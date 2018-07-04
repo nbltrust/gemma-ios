a15a25ba8560afe5816ecd6bcee218a7659fcc19
koofrank <koofranker@gmail.com>
2018-07-04 13:24:49 +0800

    add constant file

---
+++ EOS/Constants/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  EOS
+//
+//  Created by koofrank on 2018/7/4.
+//  Copyright © 2018年 com.nbltrust. All rights reserved.
+//
+
+import Foundation
+
+struct AppConfiguration {
+    static let APPID = ""
+}
