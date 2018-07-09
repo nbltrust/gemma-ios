3ad7ff714a8e22f7ab486c0ea8ed57b72d67e65a
peng zhu <891500387@qq.com>
2018-07-09 18:53:10 +0800

    add returnAction and replace checkbox images

---
+++ EOS/ViewControllers/Entry/View/Button.swift
@@ -0,0 +1,21 @@
+//
+//  Button.swift
+//  EOS
+//
+//  Created by peng zhu on 2018/7/9.
+//  Copyright © 2018年 com.nbltrust. All rights reserved.
+//
+
+import UIKit
+
+class Button: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
