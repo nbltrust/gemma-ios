9bcde5fb64e4a52ffacf731cd3f9f4d26bbac67c
koofrank <koofranker@gmail.com>
2018-07-10 14:39:23 +0800

    添加底层库 transaction 操作

--- EOS/Core/eos/EOSIO.mm
+++ EOS/Core/eos/EOSIO.mm
@@ -27,5 +27,12 @@
     return @(get_private_key(cypher.UTF8String, password.UTF8String).c_str());
 }
 
++ (NSString *)getAbiJsonString:(NSString *)code action:(NSString *)action from:(NSString *)from to:(NSString *)to quantity:(NSString *)quantity {
+    return @(create_abi_req(code.UTF8String, action.UTF8String, from.UTF8String, to.UTF8String, quantity.UTF8String).c_str());
+}
+
++ (NSString *)getTransaction:(NSString *)privateKey code:(NSString *)code from:(NSString *)from to:(NSString *)to quantity:(NSString *)quantity memo:(NSString *)memo getinfo:(NSString *)getinfo abistr:(NSString *)abistr {
+    return @(signTransaction(privateKey.UTF8String, code.UTF8String, from.UTF8String, to.UTF8String, quantity.UTF8String, memo.UTF8String, getinfo.UTF8String, abistr.UTF8String, 0, 0).c_str());
+}
 
 @end
