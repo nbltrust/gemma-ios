47cf36cc7e59ba097f54c47f64cbb758bdb623e0
朱宋宇 <370745024@qq.com>
2018-07-18 14:25:04 +0800

    截屏检测弹框UI

---
+++ EOS/UIComponents/ScreenShotAlertView.swift
@@ -0,0 +1,96 @@
+//
+//  ScreenShotAlertView.swift
+//  EOS
+//
+//  Created by zhusongyu on 2018/7/18.
+//  Copyright © 2018年 com.nbltrust. All rights reserved.
+//
+
+import Foundation
+
+@IBDesignable
+class ScreenShotAlertView: UIView {
+    
+    
+    @IBOutlet weak var sureView: Button!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var tipsLabel: UILabel!
+    
+    enum ScreenShotEvent: String {
+        case sureShot
+    }
+    
+    var tips = "" {
+        didSet {
+            tipsLabel.text = tips
+        }
+    }
+    
+    var buttonTitle = "" {
+        didSet {
+            sureView.title = buttonTitle
+        }
+    }
+    
+    func setUp() {
+        setUpUI()
+        setupEvent()
+        updateHeight()
+    }
+    
+    func setUpUI() {
+    }
+    
+    func setupEvent() {
+        sureView.button.rx.controlEvent(.touchUpInside).subscribe(onNext: {[weak self] touch in
+            guard let `self` = self else { return }
+            self.sendEventWith(ScreenShotEvent.sureShot.rawValue, userinfo: [ : ])
+            }, onError: nil, onCompleted: nil, onDisposed: nil).disposed(by: disposeBag)
+    }
+    
+    override var intrinsicContentSize: CGSize {
+        return CGSize.init(width: UIViewNoIntrinsicMetric,height: dynamicHeight())
+    }
+    
+    fileprivate func updateHeight() {
+        layoutIfNeeded()
+        self.height = dynamicHeight()
+        invalidateIntrinsicContentSize()
+    }
+    
+    fileprivate func dynamicHeight() -> CGFloat {
+        let lastView = self.subviews.last?.subviews.last
+        return lastView?.bottom ?? 0
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        layoutIfNeeded()
+        
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        loadViewFromNib()
+        setUp()
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        loadViewFromNib()
+        setUp()
+    }
+    
+    fileprivate func loadViewFromNib() {
+        let bundle = Bundle(for: type(of: self))
+        let nibName = String(describing: type(of: self))
+        let nib = UINib.init(nibName: nibName, bundle: bundle)
+        let view = nib.instantiate(withOwner: self, options: nil).first as! UIView
+        //        self.insertSubview(view, at: 0)
+        
+        addSubview(view)
+        view.frame = self.bounds
+        view.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+    }
+}
