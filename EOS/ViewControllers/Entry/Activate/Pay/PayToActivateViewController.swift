9f5e8ccd9258ea8e925da20f986e62ecb21bedc9
朱宋宇 <370745024@qq.com>
2018-09-20 11:13:41 +0800

    移除监听didBecomeActiveNotification

--- EOS/ViewControllers/Entry/Activate/Pay/PayToActivateViewController.swift
+++ EOS/ViewControllers/Entry/Activate/Pay/PayToActivateViewController.swift
@@ -31,14 +31,19 @@ class PayToActivateViewController: BaseViewController,IndicatorInfoProvider {
     }
     
     override func refreshViewController() {
+
+
+    }
+    
+    @objc func refreshPage() {
+        NotificationCenter.default.removeObserver(self, name: UIApplication.didBecomeActiveNotification, object: nil)
         switch self.coordinator?.state.pageState.value {
         case .loading?:
             self.endLoading()
-            //支付未完成
+        //支付未完成
         default:
             break
         }
-
     }
     
     func setupUI() {
@@ -92,7 +97,7 @@ extension PayToActivateViewController {
     
     @objc func NextClick(_ data:[String :Any]) {
         self.coordinator?.state.pageState.accept(.loading)
-        NotificationCenter.default.addObserver(self, selector: #selector(refreshViewController), name: UIApplication.didBecomeActiveNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(refreshPage), name: UIApplication.didBecomeActiveNotification, object: nil)
         self.coordinator?.initOrder(completion: { (success) in
             
         })
