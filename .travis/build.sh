761d99d21a447e16b399d850f7de17235537cb69
koofrank <koofranker@gmail.com>
2018-11-28 18:38:04 +0800

    添加测试fir的地址

--- .travis/build.sh
+++ .travis/build.sh
@@ -4,6 +4,8 @@ if [[ $TRAVIS_BRANCH == 'master' ]];then
 	fastlane publish
 elif [[ $TRAVIS_BRANCH == 'testflight' ]];then
 	fastlane test
+elif [[ $TRAVIS_BRANCH == 'firtest' ]];then
+	fastlane firtest
 else
 	fastlane fir
 fi
